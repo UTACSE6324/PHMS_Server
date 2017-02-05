e9a2b76b15c12e99c5b342c7e627f539e1f5432d
JarvisZhangUTA <zhangjw.uta@gmail.com>
2017-02-05 13:31:27 -0600

    Update dbconn.php

--- dbconn.php
+++ dbconn.php
@@ -7,15 +7,20 @@
 
 <?php
   header('content-type:text/html;charset=utf-8');
-
+  
+   print_r("table user info:");
+  
   try{
     $db = new PDO('mysql:host=localhost;dbname=phms','root','qgk112358');
+    $rs = $pdo -> query("select * from user"); 
+      while($row = $rs -> fetch())
+        print_r($row); 
+    
   }catch(PDOException $e){
     print "ERROR!:".$e->getMessage()."</br>";
     die();
   }
-  
-  print("database connected!");
+ 
 ?>
 </body>
 </html>
