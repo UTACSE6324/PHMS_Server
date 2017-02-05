2737cddf0d0487e66c2f0ab61a7e257e044b9038
JarvisZhangUTA <zhangjw.uta@gmail.com>
2017-02-05 13:52:50 -0600

    Update dbconn.php

--- dbconn.php
+++ dbconn.php
@@ -5,22 +5,46 @@
 </head>
 <body>
 
-<?php
-  header('content-type:text/html;charset=utf-8');
+  table user info:<br>
   
-  print_r("table user info:");
+  <table border="1">
   
-  try{
-    $pdo = new PDO('mysql:host=localhost;dbname=phms','root','qgk112358');
-    $rs = $pdo -> query("select * from user"); 
-      while($row = $rs -> fetch())
-        print_r($row); 
+    <tr>
+      <th>uid</th><th>name</th><th>password</th><th>gender</th><th>age</th><th>weight</th>
+      <th>height</th><th>bp</th><th>bsl</th><th>chol</th><th>token</th>
+    </tr>
     
-  }catch(PDOException $e){
-    print "ERROR!:".$e->getMessage()."</br>";
-    die();
-  }
- 
-?>
+    <?php
+      header('content-type:text/html;charset=utf-8');
+
+      try{
+        $pdo = new PDO('mysql:host=localhost;dbname=phms','root','qgk112358');
+        $rs = $pdo -> query("select * from user"); 
+        
+          while($row = $rs -> fetch()){
+            <tr>
+              <th>print(array_search("uid",$row));</th>
+              <th>print(array_search("name",$row));</th>
+              <th>print(array_search("password",$row));</th>
+              <th>print(array_search("gender",$row));</th>
+              <th>print(array_search("age",$row));</th>
+              <th>print(array_search("weight",$row));</th>
+              <th>print(array_search("height",$row));</th>
+              <th>print(array_search("bp",$row));</th>
+              <th>print(array_search("bsl",$row));</th>
+              <th>print(array_search("chol",$row));</th>
+              <th>print(array_search("token",$row));</th>
+            </tr>
+          }
+
+      }catch(PDOException $e){
+        print "ERROR!:".$e->getMessage()."</br>";
+        die();
+      }
+
+    ?>
+  
+  </table>
+  
 </body>
 </html>
