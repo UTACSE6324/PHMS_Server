155b2c436d6749f076a388fcfdb44a22c8b78df8
JarvisZhangUTA <zhangjw.uta@gmail.com>
2017-02-05 02:38:21 -0600

    Update dbconn.php

--- dbconn.php
+++ dbconn.php
@@ -1,9 +1,12 @@
 <?php
-header('content-type:text/html;charset=utf-8');
-try{
-  $db = new PDO('mysql:host=localhost;dbname=phms','root','qgk112358');
-}catch(PDOException $e){
-  print "ERROR!:".$e->getMessage()."</br>";
-  die();
-}
+  header('content-type:text/html;charset=utf-8');
+
+  try{
+    $db = new PDO('mysql:host=localhost;dbname=phms','root','qgk112358');
+  }catch(PDOException $e){
+    print "ERROR!:".$e->getMessage()."</br>";
+    die();
+  }
+  
+  print("database connected!");
 ?>
