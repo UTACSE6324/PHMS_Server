b3f441472ed758b9d7972bdb6b2acba98edfced6
= <=>
2017-02-05 08:29:33 +0000

    first commit

---
+++ dbconn.php
@@ -0,0 +1,9 @@
+<?php
+header('content-type:text/html;charset=utf-8');
+try{
+  $db = new PDO('mysql:host=localhost;dbname=phms','root','qgk112358');
+}catch(PDOException $e){
+  print "ERROR!:".$e->getMessage()."</br>";
+  die();
+}
+?>
