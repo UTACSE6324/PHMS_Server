5775ce93f243e55b2b19b8ec5e0e1f3b5e135f85
JarvisZhangUTA <zhangjw.uta@gmail.com>
2017-02-06 15:46:18 -0600

    Update register.php

--- register.php
+++ register.php
@@ -4,54 +4,30 @@
   $name = $_GET['username'];
   $password = $_GET['password'];
 
-  echo($name." ".$password);
-
   if($name == ''){
-    echo("if1");
-    
     header("Status-Code:-1");
     header("summary:Username cannot be empty");
   }else if(strlen($password) < 8){
-    echo("if2");
-    
     header("Status-Code:-1");
     header("summary:Password is too short");
   }else{
-    echo("if3");
-    
     $pdo = new PDO('mysql:host=localhost;dbname=phms','root','qgk112358');   
-    
     $rs = $pdo -> query("select * from user where name = ".$name.";"); 
     
-    echo($rs==null);
-    
-    if(sizeof($rs) > 0){
-        echo("if31");
-        
+    if($rs == null){
         header("Status-Code:-1");
         header("summary:Username exists");
-      
-        echo("if31");
     }else{
-        echo("if32");
-      
         header("Status-Code:1");
         header("summary:Success");
-      
-        echo("if32");
       
         $pdo -> exec("insert into user (name,password) values (".$name.",".$password.");");
         $rs = $pdo -> query("select * from user where name = ".$name.";"); 
       
-        echo("if32");
-      
         $strr = json_encode($rs);  
         echo($strr);
-      
-        echo("if32");
     }
   }
 
   echo("result");
-
 ?>
