ca2ddb0fae8d4bd49763cf21dba8b58f57d8d60d
JarvisZhangUTA <zhangjw.uta@gmail.com>
2017-02-06 02:31:37 -0600

    Update register.php

--- register.php
+++ register.php
@@ -1,1 +1,34 @@
+<?php
+  header('content-type:text/html;charset=utf-8');
+  
+  $name = $_POST['username'];
+  $password = $_POST['password'];
 
+  if($name == ''){
+    header("Status-Code:-1");
+    header("summary:Username cannot be empty");
+  }else if(strlen($password) < 8){
+    header("Status-Code:-1");
+    header("summary:Password is too short");
+  }else{
+    $pdo = new PDO('mysql:host=localhost;dbname=phms','root','qgk112358');   
+    
+    $rs = $pdo -> query("select * from user where name = ".$name.";"); 
+    
+    if(sizeof($rs) > 0){
+        header("Status-Code:-1");
+        header("summary:Username exists");
+    }else{
+        header("Status-Code:1");
+        header("summary:Success");
+      
+        $pdo -> exec("insert into user (name,password) values (".$name.",".$password.");");
+        $rs = $pdo -> query("select * from user where name = ".$name.";"); 
+      
+        $strr = json_encode($rs);  
+        echo($strr);
+    }
+   
+  }
+
+?>
