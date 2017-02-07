51e16ed8723300692ce11a04a4bb6867f66deeb1
JarvisZhangUTA <zhangjw.uta@gmail.com>
2017-02-07 14:29:47 -0600

    Update update_server.php

--- update_server.php
+++ update_server.php
@@ -1,6 +1,4 @@
 <?php
   header("Content-type: text/html; charset=utf-8");
-  
-  exec('cd /var/www/html/  git pull origin master');//进入目录
-  exec("git pull origin master");//进行git拉取，前提是使用了ssh
+  exec('cd /var/www/html/  git pull origin master');
 ?>
