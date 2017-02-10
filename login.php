db64013d3801dd6dd38b1d726b5282512b3d9db3
JarvisZhangUTA <zhangjw.uta@gmail.com>
2017-02-10 05:42:14 -0600

    Update login.php

--- login.php
+++ login.php
@@ -6,13 +6,13 @@
   $token = time();
 
   $arr = "";
-  
+
   $pdo = new PDO('mysql:host=localhost;dbname=phms','root','qgk112358'); 
-  $res = $pdo -> query("select * from user where email = '$email';") -> fetch();
+  $res = $pdo -> query("select * from user where email = '$email' and password = '$password';") -> fetch();
   
   print_r($res);
 
-  if(strlen($res) > 0 && $res['password']==$password){
+  if(strlen($res) > 0){
     header("Status-Code:1");
     header("summary:Success");
     
