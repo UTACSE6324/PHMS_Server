665cdfc6b5f4eaa62ef8c1f3d38704be898db8bf
JarvisZhangUTA <zhangjw.uta@gmail.com>
2017-02-10 05:15:51 -0600

    Update register.php

--- register.php
+++ register.php
@@ -22,12 +22,10 @@
     
   }else{
     echo("start database");
-    echo("insert into user (email,name,password,sq,sqanswer,token) values 
-                          ('$email','$name','$password','$sq','$sqanswer','$token');");
+    echo("insert into user (email,name,password,sq,sqanswer,token) values ('$email','$name','$password','$sq','$sqanswer','$token');");
     
     $pdo = new PDO('mysql:host=localhost;dbname=phms','root','qgk112358'); 
-    $ins = $pdo -> exec("insert into user (email,name,password,sq,sqanswer,token) values 
-                          ('$email','$name','$password','$sq','$sqanswer','$token');");
+    $ins = $pdo -> exec("insert into user (email,name,password,sq,sqanswer,token) values ('$email','$name','$password','$sq','$sqanswer','$token');");
     
     echo($ins);
     
