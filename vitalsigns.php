7da178ca23809954d8c77d538f504e0f50b17019
JarvisZhangUTA <zhangjw.uta@gmail.com>
2017-02-11 02:30:05 -0600

    Create vitalsigns.php

---
+++ vitalsigns.php
@@ -0,0 +1,49 @@
+<?php
+  header('content-type:text/html;charset=utf-8');
+  
+  $uid = $_GET['uid'];
+  $token = $_GET['token'];
+  
+  $age = $_GET['age'];
+  $gender = $_GET['gender'];
+  $weight = $_GET['weight'];
+  $height = $_GET['height'];
+  $bp = $_GET['bp'];
+  $bsl = $_GET['bsl'];
+  $chol = $_GET['chol'];
+  
+  $arr = "";
+  
+  $pdo = new PDO('mysql:host=localhost;dbname=phms','root','qgk112358'); 
+  $res = $pdo -> query("select * from user where uid = '$uid' and token = '$token';") -> fetch();
+  
+  if(!empty($res)){
+    $ins = $pdo -> exec("update user set age = $age,gender = $gender,weight = $weight,height=$height,
+      bp = $bp,bsl = $bsl,chol = $chol where uid = '$uid'");
+    
+    if($ins == 1){
+      header("Status-Code:1");
+      header("summary:Success");
+      
+      $arr = array(
+        'uid' => $res['uid'],
+        'age' => $res['age'],
+        'gender' => $res['gender'],
+        'weight' => $res['weight'],
+        'height' => $res['height'],
+        'bp' => $res['bp'],
+        'bsl' => $res['bsl'],
+        'chol' => $res['chol']
+      );
+      
+    }else{
+      header("Status-Code:-1");
+      header("summary:Insert invalid");
+    }
+  }else{
+    header("Status-Code:-1");
+    header("summary:Token out of date");
+  }
+  
+  echo json_encode($arr);
+?>
