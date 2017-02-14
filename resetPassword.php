d4e19de9f5dcf715ba567821522c5c8d2a037ae6
linc0814 <yzlinc0814@gmail.com>
2017-02-14 09:31:09 -0600

    add ResetPassword feature.

---
+++ resetPassword.php
@@ -0,0 +1,52 @@
+<?php
+header('content-type:text/html;charset=utf-8');
+
+$email = $_GET['email'];
+//$name = $_GET['name'];
+$password = $_GET['password'];
+$sq = $_GET['sq'];
+$sqanswer = $_GET['sqanswer'];
+$token = time();
+$arr = "";
+
+if($email == ''||$password == ''||$sq == ''||$sqanswer == ''){
+
+    header("Status-Code:-1");
+    header("summary:Cannot be empty");
+
+}else if(strlen($password) < 8){
+
+    header("Status-Code:-1");
+    header("summary:Password is too short");
+
+}else{
+
+    $pdo = new PDO('mysql:host=localhost;dbname=phms','root','qgk112358');
+    $ins = $pdo -> exec("update user set password='$password' where email = '$email' and sq = '$sq' and sqanswer = '$sqanswer';");
+
+    if($ins == 1){
+        header("Status-Code:1");
+        header("summary:Success");
+
+        $res = $pdo -> query("select * from user where email = '$email';") -> fetch();
+
+        $arr = array(
+            'uid' => $res['uid'],
+            'email' => $res['email'],
+            'name' => $res['name'],
+            'password' => $res['password'],
+            'sq' => $res['sq'],
+            'sqanswer' => $res['sqanswer'],
+            'token' => $res['token']
+        );
+
+    }else{
+        header("Status-Code:-1");
+        header("summary: Reset password failed. (Email does not exist or the answer does not match the sq_question) ");
+    }
+
+}
+
+echo json_encode($arr);
+
+?>
