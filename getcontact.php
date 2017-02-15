9a8fda33d8869b2045422ecf5c703724d896ec95
JarvisZhangUTA <zhangjw.uta@gmail.com>
2017-02-15 12:14:19 -0600

    Update getcontact.php

--- getcontact.php
+++ getcontact.php
@@ -11,7 +11,7 @@
   if(!empty($res)){
     header("Status-Code:1");
     header("summary:Success");
-    $ins = $pdo -> query("select * from contact where uid = '$uid';");
+    $ins = $pdo -> query("select * from contact where uid = '$uid';") -> fetch();
     echo json_encode($ins);
     if(!empty($ins)){
       $num = count($ins);
