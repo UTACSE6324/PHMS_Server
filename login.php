aa07e3f9b96bbf12486bf914b96d4614c3e54e13
JarvisZhangUTA <zhangjw.uta@gmail.com>
2017-02-15 22:30:45 -0600

    Update login.php

--- login.php
+++ login.php
@@ -5,8 +5,6 @@
   $password = $_GET['password'];
   $token = time();
 
-  $arr = "";
-
   $pdo = new PDO('mysql:host=localhost;dbname=phms','root','qgk112358'); 
   $res = $pdo -> query("select * from user where email = '$email' and password = '$password';") -> fetch();
 
