7830a8b33dee2737952303fc988490a164a73db0
JarvisZhangUTA <zhangjw.uta@gmail.com>
2017-02-15 22:43:53 -0600

    Update addcontact.php

--- addcontact.php
+++ addcontact.php
@@ -8,29 +8,24 @@
   $phone = $_GET['phone'];
   $email = $_GET['email'];
   
-  $arr = "";
-  
   $pdo = new PDO('mysql:host=localhost;dbname=phms','root','qgk112358'); 
   $res = $pdo -> query("select * from user where uid = '$uid' and token = '$token';") -> fetch();
-   $arr = $arr."a";
+
   if(!empty($res)){
     $ins = $pdo -> exec("insert into contact (uid, name, phone, email) values ('$uid','$name','$phone','$email');");
-    $arr = $arr."a";
+
     if($ins == 1){
       header("Status-Code:1");
       header("summary:Success");
-      $arr = $arr."a";
       $arr = "Success";
     }else{
       header("Status-Code:-1");
       header("summary:Insert invalid");
-      $arr = $arr."b";
-      $arr = "Fail";
+      $arr = $pdo->errorInfo();
     }
   }else{
     header("Status-Code:-1");
     header("summary:Token out of date");
-    $arr = $arr."c";
     $arr = "Token out of date";
   }
   
