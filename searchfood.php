8358516c81c7a153bc5385c9023e08be910c3c49
JarvisZhangUTA <zhangjw.uta@gmail.com>
2017-02-26 05:48:33 -0600

    Update searchfood.php

--- searchfood.php
+++ searchfood.php
@@ -4,19 +4,57 @@
   $query = $_GET['query'];
   
   echo $query;
-
-  //post方式
-  $curlPost = "appId=b871bf7e&appKey=8aa879546b2064de87ebc15334754bab&fields=['item_name','Cnf_calories']&query='$query'"; 
-  $ch=curl_init(); 
-  curl_setopt($ch,CURLOPT_URL,'https://api.nutritionix.com/v1_1/search?'); 
-  curl_setopt($ch,CURLOPT_HEADER,0); 
-  curl_setopt($ch,CURLOPT_RETURNTRANSFER,0); 
-  //设置是通过post还是get方法
-  curl_setopt($ch,CURLOPT_POST,1); 
-  //传递的变量
-  curl_setopt($ch,CURLOPT_POSTFIELDS,$curlPost); 
-  $data = curl_exec($ch);
-  curl_close($ch); 
+  
+  $params = array(
+    "appId" => "b871bf7e",
+    "appKey" => "8aa879546b2064de87ebc15334754bab",
+    "fields" => ["item_name",
+              "nf_calories"],
+    "query" => $query
+  );
+  
+  $header = array(
+    "content-type" => "application/json"" 
+  );
+  
+  $data = http("https:\/\/api.nutritionix.com/v1_1/search?",$params,'POST',$header);
   
   echo $data;
+
+  protected function http($url, $params, $method = 'GET', $header = array(), $multi = false) {
+        $opts = array(CURLOPT_TIMEOUT => 30, 
+                      CURLOPT_RETURNTRANSFER => 1, 
+                      CURLOPT_SSL_VERIFYPEER => false, 
+                      CURLOPT_SSL_VERIFYHOST => false, 
+                      CURLOPT_HTTPHEADER => $header);
+
+        /* 根据请求类型设置特定参数 */
+        switch(strtoupper($method)) {
+            case 'GET' :
+                $opts[CURLOPT_URL] = $url . '&' . http_build_query($params);
+                dump($opts[CURLOPT_URL]);
+                break;
+            case 'POST' :
+                //判断是否传输文件
+                $params = $multi ? $params : http_build_query($params);
+                $opts[CURLOPT_URL] = $url;
+                
+                dump($opts[CURLOPT_URL]);
+                $opts[CURLOPT_POST] = 1;
+                $opts[CURLOPT_POSTFIELDS] = $params;
+                break;
+            default :
+                throw new Exception('不支持的请求方式！');
+        }
+
+        /* 初始化并执行curl请求 */
+        $ch = curl_init();
+        curl_setopt_array($ch, $opts);
+        $data = curl_exec($ch);
+        $error = curl_error($ch);
+        curl_close($ch);
+        if ($error)
+            throw new Exception('请求发生错误：' . $error);
+        return $data;
+    }
 ?>
