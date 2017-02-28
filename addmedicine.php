670081fe843cf88502b9c1b5820919c852453e0e
Tushar Garud <tushar.garud@outlook.com>
2017-02-28 12:17:18 -0600

    Update addmedicine.php

--- addmedicine.php
+++ addmedicine.php
@@ -21,10 +21,7 @@ $contacts = $_GET['contacts'];
   $res = $pdo -> query("select * from user where uid = '$uid' and token = '$token';") -> fetch();
 
   if(!empty($res)){
-    
-    $buffer = base64_decode($image);
-    $buffer = $pdo->real_escape_string($buffer);
-    
+ 
     $ins = $pdo -> exec("insert into medicine (uid, mid, name, reminder, times, days, start_date, end_date, 
                           quantity, unit, instructions, notification, contacts) 
                           values ('$uid','$mid','$name','$reminder','$times','$days','$start_date','$end_date',
@@ -33,17 +30,16 @@ $contacts = $_GET['contacts'];
     if($ins == 1){
       header("Status-Code:1");
       header("summary:Success");
-      $arr = "Success";
+      echo $pdo->lastInsertId();
     }else{
       header("Status-Code:-1");
       header("summary:Insert invalid");
-      $arr = $pdo->errorInfo();
+      echo $pdo->errorInfo();
     }
   }else{
     header("Status-Code:-1");
     header("summary:Token out of date");
-    $arr = "Token out of date";
+    echo "Token out of date";
   }
   
-  echo json_encode($arr);
 ?>
