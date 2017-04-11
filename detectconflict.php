b5d6f345f1be16351e5f985d68f534bf1d543212
JarvisZhangUTA <zhangjw.uta@gmail.com>
2017-04-10 21:49:49 -0500

    Update detectconflict.php

--- detectconflict.php
+++ detectconflict.php
@@ -1,4 +1,17 @@
 <?php
 
-  $ins = $pdo -> query("select * from contact where uid = '$uid';") -> fetchAll();
+  $ins = $pdo -> query("select apiID from medicine where uid = '$uid';") -> fetchAll();
+  
+  $url='https://rxnav.nlm.nih.gov/REST/interaction/interaction.json?rxcui=';
+
+  $num = count($ins);
+  if($num > 1){
+    for ($i = 0; $i < $num; ++$i) {
+      $url = $url.$ins[$i]['apiID'].'+';
+    }
+    
+    $res = file_get_contents($url);
+    
+    $pdo -> query("insert into notice (uid,isnew,summary) values ('1','1','$res');")
+  }
 ?>
