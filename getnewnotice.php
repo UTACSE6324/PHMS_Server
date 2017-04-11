7c8a26f2665e34b232e239c3f6bb7d7a3c80fcf8
JarvisZhangUTA <zhangjw.uta@gmail.com>
2017-04-11 00:13:22 -0500

    Create getnewnotice.php

---
+++ getnewnotice.php
@@ -0,0 +1,20 @@
+<?php
+  header('content-type:text/html;charset=utf-8');
+  
+  $uid = $_GET['uid'];
+  $token = $_GET['token'];
+  
+  $pdo = new PDO('mysql:host=localhost;dbname=phms','root','qgk112358'); 
+  $res = $pdo -> query("select * from user where uid = '$uid' and token = '$token';") -> fetch();
+  
+  if(!empty($res)){
+    $ins = $pdo -> exec("select count(*) from notice where uid = '$uid' and isnew = 1;") -> fetch();
+    $arr = $ins('count(*)');
+  }else{
+    header("Status-Code:-1");
+    header("summary:Token out of date");
+    $arr = "0";
+  }
+  
+  echo json_encode($arr);
+?>
