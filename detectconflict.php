36089a3094370266d8c9dd9234df5fced5299c03
JarvisZhangUTA <zhangjw.uta@gmail.com>
2017-04-10 22:24:36 -0500

    Update detectconflict.php

--- detectconflict.php
+++ detectconflict.php
@@ -13,8 +13,13 @@
     $apiRes = file_get_contents($url);
     $apiRes = json_decode($apiRes, true);
     
+    var_dump($apiRes);
+    
     $conflict = $apiRes['fullInteractionTypeGroup']['fullInteractionType']['comment'];
   
+    echo '<br>';
+    echo $conflict;
+    
     if($conflict!=null)
     $res = $pdo -> query("insert into notice (uid,isnew,summary) values ('1','1','$conflict')");
   }
