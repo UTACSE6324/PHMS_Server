e0c390ea75efcf4bda6ba404ed38a2d1a314d07a
JarvisZhangUTA <zhangjw.uta@gmail.com>
2017-04-23 03:12:18 -0500

    Update generatedietanalysis.php

--- generatedietanalysis.php
+++ generatedietanalysis.php
@@ -1,25 +1,25 @@
-<?php
-	echo "<html>";
-	echo "<head>";
-	echo "<script type='text/javascript' src='https://www.gstatic.com/charts/loader.js'></script>";
-	echo "<script type='text/javascript'>";
-	echo "google.charts.load('current', {packages: ['corechart']});";
-	echo "google.charts.setOnLoadCallback(drawChart);"
-	echo "function drawChart() {
-			  var data = new google.visualization.DataTable();
-			  data.addColumn('string', 'Element');
-			  data.addColumn('number', 'Percentage');
-			  data.addRows([
-				['Nitrogen', 0.78],
-				['Oxygen', 0.21],
-				['Other', 0.01]
-			  ]);
-			  var chart = new google.visualization.PieChart(document.getElementById('dietPieChart'));
-			  chart.draw(data, null);
-		}";
-	echo "</script>"
-	echo "</head>";
-	echo "<body>";
-	echo "</body>";
-	echo "</html>";
-?>
+<html>
+<head>
+  <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+  <script type="text/javascript">
+    google.charts.load('current', {packages: ['corechart']});
+    google.charts.setOnLoadCallback(drawChart);
+    function drawChart() {
+      // Define the chart to be drawn.
+      var data = new google.visualization.DataTable();
+      data.addColumn('string', 'Element');
+      data.addColumn('number', 'Percentage');
+      data.addRows([
+        ['Nitrogen', 0.78],
+        ['Oxygen', 0.21],
+        ['Other', 0.01]
+      ]);
+      var chart = new google.visualization.PieChart(document.getElementById('dietPieChart'));
+      chart.draw(data, null);
+    }
+  </script>
+</head>
+<body>
+  <div id="dietPieChart"/>
+</body>
+</html>
