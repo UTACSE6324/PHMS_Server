a09564b8be8a6bf4132c697597d837afbb4ed3c1
JarvisZhangUTA <zhangjw.uta@gmail.com>
2017-04-23 04:32:35 -0500

    Update search.php

--- search.php
+++ search.php
@@ -13,9 +13,9 @@
   $arr = array();
   $pdo = new PDO('mysql:host=localhost;dbname=phms','root','qgk112358'); 
   $res = $pdo -> query("select * from user where uid = '$uid' and token = '$token';") -> fetch();
-  echo $res;
-  /*
+
   if(!empty($res)){
+    /*
     header("Status-Code:1");
     header("summary:Success");
         
@@ -106,12 +106,12 @@
       header("Status-Code:-1");
       header("summary:Insert invalid");
     }
-    
+    */
   }else{
     header("Status-Code:-1");
     header("summary:Token out of date");
   }
   
   echo json_encode($arr);
-  */
+  
 ?>
