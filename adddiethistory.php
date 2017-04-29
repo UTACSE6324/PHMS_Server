1b2986b92e1b685b9fff088f6e87c91d258957fc
JarvisZhangUTA <zhangjw.uta@gmail.com>
2017-04-29 17:56:49 -0500

    Update adddiethistory.php

--- adddiethistory.php
+++ adddiethistory.php
@@ -38,12 +38,12 @@
     $dietid = $pdo->lastInsertId();
     
     $apiID = $_GET['apiid'];
+    echo $apiID."\n";
     $url='https://trackapi.nutritionix.com/v2/search/item?
             x-app-key=8aa879546b2064de87ebc15334754bab&x-app-id=b871bf7e&nix_item_id='.$apiID;
     $apiRes = file_get_contents($url);
     $apiRes = json_decode($apiRes, true);
-    
-    echo $apiRes;
+   
     
     $fat = $apiRes['foods'][0]['nf_total_fat'];
     $protein = $apiRes['foods'][0]['nf_protein'];
