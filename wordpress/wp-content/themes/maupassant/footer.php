e196a8e9a564b32a2a027ddd12c18d639a1b96aa
JarvisZhangUTA <zhangjw.uta@gmail.com>
2017-07-19 04:29:03 -0500

    Update footer.php

--- wordpress/wp-content/themes/maupassant/footer.php
+++ wordpress/wp-content/themes/maupassant/footer.php
@@ -4,7 +4,7 @@
 <footer id="footer">
 	<div class="container">
 		&copy; <?php echo date('Y'); ?> <a href="<?php bloginfo('url'); ?>"><?php bloginfo('name'); ?></a>.
-		<?php _e('由 <a href="http://wordpress.org">Wordpress</a> 强力驱动. 模板由<a href="http://pagecho.com">cho</a>制作'); ?>.
+		<?php _e('模板由<a href="http://pagecho.com">cho</a>制作'); ?>.
 	</div>
 </footer>
 <?php wp_footer();?>
