96ee1a9c3f58ccb333133564bdc6da5b883e7fd6
JarvisZhangUTA <zhangjw.uta@gmail.com>
2017-07-19 04:31:17 -0500

    Update index.php

--- wordpress/wp-content/themes/maupassant/index.php
+++ wordpress/wp-content/themes/maupassant/index.php
@@ -8,12 +8,11 @@
 				</h2>
 				<ul class="post-meta">
 					<li><?php the_time('F j, Y'); ?></li>
-					<li class="comment-count"><?php comments_popup_link('0 条评论', ' 1 条评论', '% 条评论'); ?></li>
+					<li><a href="<?php the_permalink() ?>" rel="bookmark" title="进入<?php the_title();?>" >阅读全文</a></li>
 				</ul>
 				<div class="post-content">
 					<div class="entry">
 						<?php the_excerpt(); ?>
-						<a href="<?php the_permalink() ?>" rel="bookmark" title="进入<?php the_title();?>" >阅读全文</a>
 					</div>
 				</div>
 			</article>
