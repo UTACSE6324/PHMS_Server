89104d000f512c27d921105941d2b41987fa204f
jiawei zhang <zhangjw.uta@gmail.com>
2017-08-30 17:15:25 -0500

    Update header.php

--- wordpress/wp-content/themes/maupassant/header.php
+++ wordpress/wp-content/themes/maupassant/header.php
@@ -4,7 +4,7 @@
 <!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
 <!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
 <head>
-    <?php ms_head(); ?>
+    <?php ms_head(); wp_header();?>
 </head>
 <body>
 <header id="header" class="clearfix">
