9e12feaf57797da2e351bcae4c74566a427e158e
jiawei zhang <zhangjw.uta@gmail.com>
2017-08-30 17:12:40 -0500

    Update functions.php

--- wordpress/wp-content/themes/maupassant/functions.php
+++ wordpress/wp-content/themes/maupassant/functions.php
@@ -156,7 +156,5 @@
 			</div>
 	<?php
 	}	
-		if ( function_exists(‘register_sidebar’) )
-			register_sidebar(array(‘before_widget’ => ‘<div class=”sidebox”> ‘,‘after_widget’ => ‘</div>’,‘before_title’ => ‘<h2>’,‘after_title’ => ‘</h2>’,));
 
 ?>
